988fbb46e01e3461e5cd3294e157896d6fa13a34
Joshua McEwen <jamyt7@mail.missouri.edu>
2019-05-03 18:54:58 -0500

    start save

---
+++ FieldSurvey/SurveyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SurveyViewController.swift
+//  FieldSurvey
+//
+//  Created by McEwen, Joshua A. (MU-Student) on 5/3/19.
+//  Copyright © 2019 McEwen, Joshua A. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class SurveyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
